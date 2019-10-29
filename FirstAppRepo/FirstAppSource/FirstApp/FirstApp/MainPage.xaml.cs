d7f545f4a6843e9e6e7d732d820891ff7fa044d9
Hannah Seccia <33502113+hseccia@users.noreply.github.com>
2019-10-29 00:21:08 -0400

    Uploading the files to github

---
+++ FirstAppRepo/FirstAppSource/FirstApp/FirstApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int counter = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            
+            counter++;
+            (sender as Button).Text = $"You clicked {counter} times";
+        }
+    }
+}
